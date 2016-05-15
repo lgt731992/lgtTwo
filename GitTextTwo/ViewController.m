0382d0a1a050bda8cf989f25acc54ab44a0d0ab9
lgt731992 <lgtwykey@126.com>
2016-05-15 11:31:44 +0800

    Update ViewController.m

--- GitTextTwo/ViewController.m
+++ GitTextTwo/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"fdfsdfds");
+    NSLog(@"fdsfdsf111111111");
 }
 
 - (void)didReceiveMemoryWarning {
